e905df0246380cff5a1c797089a50d3de82be01d
Alex Dunne <alex.dunne@gymshark.com>
2021-09-14 14:33:10 +0100

    Added endpoints to fetch different slices of the stored data

---
+++ cmd/api/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/alexdunne/gs-onboarding/hn"
+	"github.com/alexdunne/gs-onboarding/inmem"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type ItemStore interface {
+	GetAll() hn.Items
+	GetStories() hn.Items
+	GetJobs() hn.Items
+}
+
+func main() {
+	store := inmem.NewInMemoryItemStore()
+
+	server := NewServer(store)
+	server.start("localhost:8000")
+}
+
+type server struct {
+	router *echo.Echo
+	store  ItemStore
+}
+
+func NewServer(store ItemStore) *server {
+	s := &server{
+		store: store,
+	}
+
+	s.router = echo.New()
+	s.router.Use(
+		middleware.Recover(),
+		middleware.Logger(),
+	)
+
+	s.router.GET("/all", s.handleGetAllItems)
+	s.router.GET("/stories", s.handleGetStories)
+	s.router.GET("/jobs", s.handleGetJobs)
+
+	return s
+}
+
+func (s *server) start(address string) {
+	err := s.router.Start(address)
+	s.router.Logger.Fatal(err)
+}
+
+func (s *server) handleGetAllItems(c echo.Context) error {
+	items := s.store.GetAll()
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"items": items,
+	})
+}
+
+func (s *server) handleGetStories(c echo.Context) error {
+	items := s.store.GetStories()
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"items": items,
+	})
+}
+
+func (s *server) handleGetJobs(c echo.Context) error {
+	items := s.store.GetJobs()
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"items": items,
+	})
+}
