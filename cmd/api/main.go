ef7a40eac0d463eeea58a4bce52a5c8dcd070309
Alex Dunne <alex.dunne@gymshark.com>
2021-09-14 20:00:23 +0100

    Moved api address to an env variable

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/alexdunne/gs-onboarding/hn"
 	"github.com/alexdunne/gs-onboarding/internal/memory"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
 )
+
+type Config struct {
+	Addr string
+}
+
+func loadConfig() (*Config, error) {
+	viper.SetConfigFile(".env")
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, errors.Wrap(err, "failed to read env file")
+	}
+
+	return &Config{
+		Addr: viper.GetString("ADDR"),
+	}, nil
+}
 
 type ItemStore interface {
 	GetAll() hn.Items
@@ -16,10 +34,16 @@ type ItemStore interface {
 }
 
 func main() {
+	cfg, err := loadConfig()
+	if err != nil {
+		panic(err)
+		os.Exit(1)
+	}
+
 	store := memory.NewItemStore()
 
 	server := NewServer(store)
-	server.start("localhost:8000")
+	server.start(cfg.Addr)
 }
 
 type server struct {
