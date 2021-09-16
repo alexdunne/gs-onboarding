4abdaed71905a6674fbbfe56111d9026e31aa1bc
Alex Dunne <alex.dunne@gymshark.com>
2021-09-16 17:25:08 +0100

    Refactored api to use handlers

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,14 +4,14 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/http"
-	"os"
 
+	"github.com/alexdunne/gs-onboarding/internal/api"
 	"github.com/alexdunne/gs-onboarding/internal/database"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
 )
 
 type Config struct {
@@ -38,93 +38,42 @@ func loadConfig() (*Config, error) {
 	}, nil
 }
 
-type ItemStore interface {
-	GetAll(ctx context.Context) ([]database.Item, error)
-	GetStories(ctx context.Context) ([]database.Item, error)
-	GetJobs(ctx context.Context) ([]database.Item, error)
-}
-
 func main() {
-	if err := run(); err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-}
-
-func run() error {
 	cfg, err := loadConfig()
 	if err != nil {
-		return err
+		log.Fatal(errors.Wrap(err, "loading config"))
 	}
+
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "creating logger"))
+	}
+	defer logger.Sync()
 
 	ctx := context.Background()
 
 	db, err := database.New(ctx, cfg.DatabaseDSN)
 	if err != nil {
-		return errors.Wrap(err, "opening store db connection")
+		log.Fatal(errors.Wrap(err, "opening store db connection"))
 	}
 	defer db.Close()
 
-	server := NewServer(db)
-	return server.start(cfg.Addr)
-}
-
-type server struct {
-	router *echo.Echo
-	store  ItemStore
-}
-
-func NewServer(store ItemStore) *server {
-	s := &server{
-		store: store,
-	}
-
-	s.router = echo.New()
-	s.router.Use(
+	router := echo.New()
+	router.HideBanner = true
+	router.Use(
 		middleware.Recover(),
 		middleware.Logger(),
 	)
 
-	s.router.GET("/all", s.handleGetAllItems)
-	s.router.GET("/stories", s.handleGetStories)
-	s.router.GET("/jobs", s.handleGetJobs)
-
-	return s
-}
-
-func (s *server) start(addr string) error {
-	return s.router.Start(addr)
-}
-
-func (s *server) handleGetAllItems(c echo.Context) error {
-	items, err := s.store.GetAll(c.Request().Context())
-	if err != nil {
-		return err
+	h := api.Handler{
+		DB: db,
 	}
 
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"items": items,
-	})
-}
+	router.GET("/all", h.HandleGetAllItems)
+	router.GET("/stories", h.HandleGetStories)
+	router.GET("/jobs", h.HandleGetJobs)
 
-func (s *server) handleGetStories(c echo.Context) error {
-	items, err := s.store.GetStories(c.Request().Context())
-	if err != nil {
-		return err
+	if err := router.Start(cfg.Addr); err != nil {
+		logger.Fatal("starting server", zap.Error(err))
 	}
-
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"items": items,
-	})
-}
-
-func (s *server) handleGetJobs(c echo.Context) error {
-	items, err := s.store.GetJobs(c.Request().Context())
-	if err != nil {
-		return err
-	}
-
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"items": items,
-	})
 }
