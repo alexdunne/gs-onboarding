142059f0717a340ac93ef09ec9d75320300f3249
Alex Dunne <alex.dunne@gymshark.com>
2021-09-17 10:02:22 +0100

    Implemented a gRPC server to interact with the database hacker news items

---
+++ cmd/api/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/alexdunne/gs-onboarding/internal/api"
+	"github.com/alexdunne/gs-onboarding/internal/database"
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+type Config struct {
+	Port        int
+	DatabaseDSN string
+}
+
+func loadConfig() (*Config, error) {
+	viper.SetConfigFile(".env")
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, errors.Wrap(err, "failed to read env file")
+	}
+
+	return &Config{
+		Port: viper.GetInt("API_PORT"),
+		DatabaseDSN: fmt.Sprintf(
+			"postgres://%s:%s@%s:%s/%s",
+			viper.GetString("DATABASE_USER"),
+			viper.GetString("DATABASE_PASSWORD"),
+			viper.GetString("DATABASE_HOST"),
+			viper.GetString("DATABASE_PORT"),
+			viper.GetString("DATABASE_DB"),
+		),
+	}, nil
+}
+
+func main() {
+	cfg, err := loadConfig()
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "loading config"))
+	}
+
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "creating logger"))
+	}
+	defer logger.Sync()
+
+	ctx := context.Background()
+
+	db, err := database.New(ctx, cfg.DatabaseDSN)
+	if err != nil {
+		log.Fatal(errors.Wrap(err, "opening store db connection"))
+	}
+	defer db.Close()
+
+	h := api.Handler{
+		DB: db,
+	}
+
+	s := api.NewServer(cfg.Port, h)
+	if err := s.Start(); err != nil {
+		logger.Fatal("running server", zap.Error(err))
+	}
+}
---
+++ internal/api/handler.go
@@ -0,0 +1,71 @@
+package api
+
+import (
+	pb "github.com/alexdunne/gs-onboarding/internal/api/protobufs"
+	"github.com/alexdunne/gs-onboarding/internal/database"
+	"github.com/pkg/errors"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type Handler struct {
+	pb.UnimplementedAPIServer
+	DB database.Database
+}
+
+func (h Handler) ListAll(empty *emptypb.Empty, s pb.API_ListAllServer) error {
+	items, err := h.DB.GetAll(s.Context())
+	if err != nil {
+		return errors.Wrap(err, "fetching all items")
+	}
+
+	for _, v := range items {
+		if err := s.Send(dbItemToPbItem(v)); err != nil {
+			return errors.Wrap(err, "streaming item to client")
+		}
+	}
+
+	return nil
+}
+
+func (h Handler) ListStories(empty *emptypb.Empty, s pb.API_ListStoriesServer) error {
+	items, err := h.DB.GetStories(s.Context())
+	if err != nil {
+		return errors.Wrap(err, "fetching all items")
+	}
+
+	for _, v := range items {
+		if err := s.Send(dbItemToPbItem(v)); err != nil {
+			return errors.Wrap(err, "streaming item to client")
+		}
+	}
+
+	return nil
+}
+
+func (h Handler) ListJobs(empty *emptypb.Empty, s pb.API_ListJobsServer) error {
+	items, err := h.DB.GetJobs(s.Context())
+	if err != nil {
+		return errors.Wrap(err, "fetching all items")
+	}
+
+	for _, v := range items {
+		if err := s.Send(dbItemToPbItem(v)); err != nil {
+			return errors.Wrap(err, "streaming item to client")
+		}
+	}
+
+	return nil
+}
+
+func dbItemToPbItem(item database.Item) *pb.Item {
+	return &pb.Item{
+		Id:        int32(item.ID),
+		Type:      item.Type,
+		Content:   item.Content,
+		Url:       item.URL,
+		Score:     int32(item.Score),
+		Title:     item.Title,
+		CreatedAt: item.CreatedAt.Unix(),
+		CreatedBy: item.CreatedBy,
+	}
+}
