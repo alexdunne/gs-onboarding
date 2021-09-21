f88061129f8d20f7726672d11d2951ea796223c0
Alex Dunne <alex.dunne@gymshark.com>
2021-09-21 17:10:54 +0100

    WIP: Added tests

--- internal/database/items.go
+++ internal/database/items.go
@@ -3,6 +3,7 @@ package database
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/alexdunne/gs-onboarding/internal/models"
 	"github.com/georgysavva/scany/pgxscan"
@@ -10,6 +11,8 @@ import (
 )
 
 func (c *Client) GetAll(ctx context.Context) ([]models.Item, error) {
+	log.Println(c.pool.Ping(ctx))
+
 	var items []models.Item
 	err := pgxscan.Select(ctx, c.pool, &items, `SELECT id, type, content, url, score, title, created_at, created_by FROM items`)
 	if err != nil {
@@ -50,6 +53,7 @@ func (c *Client) GetJobs(ctx context.Context) ([]models.Item, error) {
 }
 
 func (c *Client) Write(ctx context.Context, item models.Item) error {
+	log.Println("writing")
 	sql := `
 	INSERT INTO items (id, type, content, url, score, title, created_by, created_at)
 	VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
